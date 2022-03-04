4ce81d0ac7c51ad6d61cbbaeb9bfb7147d13c008
Swarnim-Mishra <79535033+Swarnim-Mishra@users.noreply.github.com>
2022-03-04 13:53:30 +0530

    Create credentials.js

---
+++ src/firebase/credentials.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPGN00hQkkwW3RLeW2mNVSjsLIIHJnfhY",
+  authDomain: "instagram-best-fe658.firebaseapp.com",
+  projectId: "instagram-best-fe658",
+  storageBucket: "instagram-best-fe658.appspot.com",
+  messagingSenderId: "364014418335",
+  appId: "1:364014418335:web:db7bd96f263995d38525aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
