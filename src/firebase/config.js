2f405cc67935c3effa73bb835dae8f154094a61d
Swarnim-Mishra <79535033+Swarnim-Mishra@users.noreply.github.com>
2022-03-04 14:12:09 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,24 @@
 import firebase from "firebase/app";
-import firebaseConfig from "credentials";
 import "firebase/auth";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPGN00hQkkwW3RLeW2mNVSjsLIIHJnfhY",
+  authDomain: "instagram-best-fe658.firebaseapp.com",
+  projectId: "instagram-best-fe658",
+  storageBucket: "instagram-best-fe658.appspot.com",
+  messagingSenderId: "364014418335",
+  appId: "1:364014418335:web:db7bd96f263995d38525aa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+content_copy
+
 
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
